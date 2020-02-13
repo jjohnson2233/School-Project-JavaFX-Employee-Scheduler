9387fb02db76a335b63468f9f1254844d5e094ab
Jared Johnson <jaredjohnswine@icloud.com>
2020-02-13 13:22:18 -0800

    Added search functionality to the customer page.

--- src/application/Main.java
+++ src/application/Main.java
@@ -12,8 +12,9 @@ import javafx.stage.Stage;
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) throws IOException {
-		Parent root = FXMLLoader.load(getClass().getResource("../views/LoginScreen.fxml"));
-		Scene scene = new Scene(root,400,350);
+		Parent root = FXMLLoader.load(getClass().getResource("../views/CustomerScreen.fxml"));
+		//Scene scene = new Scene(root,400,350);
+		Scene scene = new Scene(root);
 		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 		primaryStage.setScene(scene);
 		primaryStage.setResizable(false);
--- src/controllers/CustomerScreenController.java
+++ src/controllers/CustomerScreenController.java
@@ -3,20 +3,22 @@ package controllers;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
 import application.DB;
 import application.ListedCustomer;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
@@ -26,34 +28,11 @@ import managers.CountryManager;
 import managers.CustomerManager;
 
 public class CustomerScreenController implements Initializable{
+	//Current list of customers
+	ObservableList<ListedCustomer> listOfCustomers;
+
 	//Object for selected item
 	public static ListedCustomer selectedItem;
-	
-	//Add appointment button
-	@FXML private Button addAppointmentButton;
-	
-	//Add appointment button action
-	private void addAppointmentButtonAction (ActionEvent event) {
-		if (customersTable.getItems().isEmpty()) {
-			Alert alert = new Alert(AlertType.ERROR);
-			alert.setHeaderText("No customer selected");
-			alert.setContentText("You must select a customer to schedule an appointment");
-			alert.showAndWait();
-		} else {
-			try {
-				selectedItem = customersTable.getSelectionModel().getSelectedItem();
-				//Go to add appointment screen
-				Parent root;
-				Stage stage = (Stage) appointmentsButton.getScene().getWindow();
-				root = FXMLLoader.load(getClass().getResource("../views/AddAppointmentScreen.fxml"));
-				Scene scene = new Scene(root);
-				stage.setScene(scene);
-				stage.show();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-	}
 	
 	//Customers Table
 		@FXML private TableView<ListedCustomer> customersTable;
@@ -63,7 +42,34 @@ public class CustomerScreenController implements Initializable{
 		@FXML private TableColumn<ListedCustomer, String> countryColumn;
 		@FXML private TableColumn<ListedCustomer, String> postalCodeColumn;
 		@FXML private TableColumn<ListedCustomer, String> phoneColumn;
-		
+
+		//Search text field
+		@FXML private TextField searchTextField;
+
+		//Search button
+		@FXML private Button searchButton;
+		//Search button action
+		private void searchButtonAction(ActionEvent event) {
+			//Get the search term
+			String searchTerm = searchTextField.getText();
+			//Get a list of the customers in the database and filter them by the search term
+			List rawList = listOfCustomers.stream()
+					.filter(customer ->
+							customer.getName().toLowerCase().contains(searchTerm.toLowerCase()) ||
+							customer.getAddress().toLowerCase().contains(searchTerm.toLowerCase()) ||
+							customer.getCity().toLowerCase().contains(searchTerm.toLowerCase()) ||
+							customer.getAddress2().toLowerCase().contains(searchTerm.toLowerCase()) ||
+							customer.getCountry().toLowerCase().contains(searchTerm.toLowerCase()) ||
+							customer.getPhone().toString().toLowerCase().contains(searchTerm.toLowerCase()) ||
+							customer.getPostalCode().toString().toLowerCase().contains(searchTerm.toLowerCase())
+					)
+					.collect(Collectors.toList());
+			//Make the list observable
+			ObservableList<ListedCustomer> observableList = FXCollections.observableList(rawList);
+			//Set the table to show the new filtered list
+			customersTable.setItems(observableList);
+		}
+
 		//Appointments List button
 		@FXML private Button appointmentsButton;
 		//Appointments list button action
@@ -80,7 +86,32 @@ public class CustomerScreenController implements Initializable{
 				e.printStackTrace();
 			}
 		}
-		
+
+		//Add appointment button
+		@FXML private Button addAppointmentButton;
+		//Add appointment button action
+		private void addAppointmentButtonAction (ActionEvent event) {
+			if (customersTable.getItems().isEmpty()) {
+				Alert alert = new Alert(AlertType.ERROR);
+				alert.setHeaderText("No customer selected");
+				alert.setContentText("You must select a customer to schedule an appointment");
+				alert.showAndWait();
+			} else {
+				try {
+					selectedItem = customersTable.getSelectionModel().getSelectedItem();
+					//Go to add appointment screen
+					Parent root;
+					Stage stage = (Stage) appointmentsButton.getScene().getWindow();
+					root = FXMLLoader.load(getClass().getResource("../views/AddAppointmentScreen.fxml"));
+					Scene scene = new Scene(root);
+					stage.setScene(scene);
+					stage.show();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
 		//Add button
 		@FXML private Button addButton;
 		//Add button action
@@ -140,15 +171,17 @@ public class CustomerScreenController implements Initializable{
 		@Override
 		public void initialize(URL arg0, ResourceBundle arg1) {
 			//Set button actions
+			searchButton.setOnAction(event -> searchButtonAction(event));
 			addAppointmentButton.setOnAction(event -> addAppointmentButtonAction(event));
 			appointmentsButton.setOnAction(event -> appointmentsButtonAction(event));
 			addButton.setOnAction(event -> addButtonAction(event));
 			editButton.setOnAction(event -> editButtonAction(event));
 			deleteButton.setOnAction(event -> deleteButtonAction(event));
 			
-			//Display data in table
+			//Get a list of customers from the database and display them in the table
 			try {
-				customersTable.setItems(DB.gatherCustomers());
+				listOfCustomers = DB.gatherCustomers();
+				customersTable.setItems(listOfCustomers);
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
